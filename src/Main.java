dd268b4906a084b327c2e97bdc7f79e3f935d425
enescanyildiz <enescanyildiz41@gmail.com>
2023-01-09 17:19:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 1; i <= n ; i++)
+        {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+
+        }
+        for (int i=0;i<=n-1; i++){
+            for (int j=n;j>=n-i;j--){
+                System.out.print(" ");
+            }
+            for (int k=n;k>=(i*2);k--){
+                System.out.print("*");
+            }
+            System.out.println(" "); }
+
+
+    }
+}
